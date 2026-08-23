dd48b864d5eb50eadc8a1ced68d9dd8c33985f3b
Ilya Shelkunov <shelkunow48@gmail.com>
2026-08-23 17:17:16 +0300

    feat: implement modal with zustand and fixed overlay

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,14 +1,17 @@
-import { CreateOrderForm } from "../components/shared/CreateOrderForm";
+import Modal from "../components/shared/Modal";
 import { OrderList } from "../components/shared/OrderList";
+import { useModalStore } from "../store/useModalStore";
 
 export function DashboardPage() {
+  const { isOpen, openModal } = useModalStore();
   return (
     <main className="flex flex-col md:flex-row items-center justify-center gap-4 text-base md:text-xl. p-5">
-      <div className="flex flex-col ">
+      {isOpen && <Modal />}
+      <div className="flex flex-col gap-4">
         <h2 className="text-center font-bold p-1 text-2xl">Dashboard</h2>
+        <button onClick={openModal}>CREATE ORDER</button>
         <OrderList />
       </div>
-      <CreateOrderForm />
     </main>
   );
 }
